6521c63b2211f1cc7fbec1ed5f4583b5ef554f51
Jordan Fox <jordan.fox@canstar.com.au>
2023-11-20 16:24:27 +1000

    image upload debugging

--- frontend/image-wizard-frontend/src/app/components/image-upload/imageUpload.tsx
+++ frontend/image-wizard-frontend/src/app/components/image-upload/imageUpload.tsx
@@ -15,6 +15,47 @@ export function ImageUpload() {
     setImages(imageList as never[]);
   };
 
+  const convertToNewFormat = async (imageData, desiredFormat, addUpdateIndex) => {
+    try {
+      const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT || 'http://image-wizard.local/api';
+      console.log(apiEndpoint)
+
+      const formData = new FormData();
+      formData.append('image', imageData);
+
+
+      const response = await fetch(`${apiEndpoint}/convert?format=${desiredFormat}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to convert image');
+      }
+
+      // Assuming the API returns the image bytes
+      const newImageBytes = await response.arrayBuffer();
+
+      // Convert the image bytes to a Data URL
+      const newDataURL = `data:image/${desiredFormat};base64,${btoa(
+        String.fromCharCode.apply(null, new Uint8Array(newImageBytes))
+      )}`;
+
+      // Update the state with the new image data
+      setImages(prevImages => {
+        const updatedImages = [...prevImages];
+        updatedImages[addUpdateIndex].dataURL = newDataURL;
+        return updatedImages;
+      });
+    } catch (error) {
+      console.error('Error converting image:', error.message);
+    }
+  };
+
+
   return (
     <div className="image-upload">
       <ReactImageUploading
@@ -32,7 +73,6 @@ export function ImageUpload() {
           isDragging,
           dragProps
         }) => (
-          // write your building UI
           <div className="upload__image-wrapper">
             <button
               style={isDragging ? { color: "red" } : undefined}
@@ -46,10 +86,13 @@ export function ImageUpload() {
             {imageList.map((image, index) => (
               <div key={index} className="image-item">
                 <img src={image.dataURL} alt="" width={100} />
-
+                <span>{image.file?.name}</span>
                 <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => convertToNewFormat(image.dataURL, 'png', 0)}>
+                    Convert to new format
+                  </button>
                   <button onClick={() => onImageRemove(index)}>Remove</button>
+                  <button onClick={() => onImageRemove(index)}>Download</button>
                 </div>
               </div>
             ))}

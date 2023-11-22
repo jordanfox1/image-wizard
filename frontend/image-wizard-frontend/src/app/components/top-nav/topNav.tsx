fa0ced565e66e114588dac671eda60cd5398052a
Jordan Fox <jordan.fox@canstar.com.au>
2023-11-22 14:32:54 +1000

    top nav

---
+++ frontend/image-wizard-frontend/src/app/components/top-nav/topNav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./topNav.css";
+
+export const TopNav = () => {
+    return (
+        <section className="top-nav">
+            <div className="logo-container">
+                <span className='eye-icon'>👁️</span>
+                <span className="top-nav-text">Image Wizard</span>
+            </div>
+        </section>
+    )
+};

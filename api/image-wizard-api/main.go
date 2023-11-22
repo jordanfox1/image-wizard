66c6ecb623df72f73aed86c58920360e14ad1a13
Jordan Fox <jordan.fox@canstar.com.au>
2023-11-22 11:42:42 +1000

    add logger

--- api/image-wizard-api/main.go
+++ api/image-wizard-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/jordanfox1/image-wizard-api/api/image-wizard-api/routes"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,6 +11,7 @@ import (
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
+	app.Use(logger.New())
 
 	routes.SetupRoutes(app)
 

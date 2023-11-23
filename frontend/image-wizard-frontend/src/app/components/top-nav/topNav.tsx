6894f29defcba957f61b896fbe8acf2a60546a16
Jordan Fox <jordan.fox@canstar.com.au>
2023-11-23 17:01:02 +1000

    update

--- frontend/image-wizard-frontend/src/app/components/top-nav/topNav.tsx
+++ frontend/image-wizard-frontend/src/app/components/top-nav/topNav.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./topNav.css";
-import { IconPhoto, IconWand } from '@tabler/icons-react';
+import { IconWand } from '@tabler/icons-react';
 
 export const TopNav = () => {
     return (
         <section className="top-nav">
             <div className="logo-container">
-                {/* <span className='eye-icon'>👁️</span> */}
                 < IconWand />
                 <span className="top-nav-text">Image Wizard</span>
             </div>

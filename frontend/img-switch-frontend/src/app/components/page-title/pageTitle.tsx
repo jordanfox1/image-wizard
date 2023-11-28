531b04863d2817ab4ce8daffb74691714ff85f07
Jordan Fox <jordan.fox@canstar.com.au>
2023-11-28 10:41:47 +1000

    update h3 to h2

--- frontend/img-switch-frontend/src/app/components/page-title/pageTitle.tsx
+++ frontend/img-switch-frontend/src/app/components/page-title/pageTitle.tsx
@@ -4,7 +4,7 @@ export default function PageTitle() {
     return (
         <div className='page-title'>
             <h1 className='heading'>Img Switch</h1>
-            <h3 className='subheading'>Free online image conversion tool</h3>
+            <h2 className='subheading'>Free online image conversion tool</h2>
         </div>
     )
 }
